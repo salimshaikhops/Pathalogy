1decd0f4ca7dcd58ed8aa4a8707364474b245c1b
salimshaikhops <83754556+salimshaikhops@users.noreply.github.com>
2021-05-07 06:44:13 +0530

    project done

---
+++ src/main/java/com/smartcontact/manager/PathologyWebApplication.java
@@ -0,0 +1,13 @@
+package com.smartcontact.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PathologyWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PathologyWebApplication.class, args);
+	}
+
+}
