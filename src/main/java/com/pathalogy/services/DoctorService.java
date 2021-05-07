30a974ba7eb3b887555a6db9154df864d6ee2e7e
salimshaikhops <83754556+salimshaikhops@users.noreply.github.com>
2021-05-07 09:00:37 +0530

    repository and service  done

--- src/main/java/com/pathalogy/services/DoctorService.java
+++ src/main/java/com/pathalogy/services/DoctorService.java
@@ -1,5 +1,15 @@
+
 package com.pathalogy.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pathalogy.repository.DoctorRepository;
+
+
+@Service
 public class DoctorService {
 
+	@Autowired
+	private DoctorRepository doctorRepository;
 }
